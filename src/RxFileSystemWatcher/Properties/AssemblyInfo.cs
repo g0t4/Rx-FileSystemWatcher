89a114696459bb0d40fd29bc542e0fc296f4a371
Wesley McClure <wes.mcclure@gmail.com>
2014-01-08 12:36:48 -0800

    Initial work on an ObservableFileSystemWatcher adapter to turn the FileSystemWatcher events into Reactive streams with the Reactive Framework.

---
+++ src/RxFileSystemWatcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RxFileSystemWatcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RxFileSystemWatcher")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ac26a3f-ea94-44ca-8131-e3a63ad35bfa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Tests/ObservableFileSystemWatcherTests.cs
@@ -0,0 +1,123 @@
+﻿namespace Tests
+{
+	using System;
+	using System.IO;
+	using System.Reactive.Linq;
+	using System.Reactive.Threading.Tasks;
+	using System.Threading.Tasks;
+	using NUnit.Framework;
+	using RxFileSystemWatcher;
+
+	[TestFixture]
+	public class ObservableFileSystemWatcherTests : AssertionHelper
+	{
+		protected string TempPath;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			TempPath = Guid.NewGuid().ToString();
+			Directory.CreateDirectory(TempPath);
+		}
+
+		[Test]
+		[Timeout(2000)]
+		public async Task WriteToFile_StreamsChanged()
+		{
+			using (var watcher = new ObservableFileSystemWatcher(c => { c.Path = TempPath; }))
+			{
+				var firstChanged = watcher.Changed.FirstAsync().ToTask();
+				watcher.Start();
+
+				File.WriteAllText(Path.Combine(TempPath, "Changed.Txt"), "foo");
+
+				var changed = await firstChanged;
+				Expect(changed.ChangeType, Is.EqualTo(WatcherChangeTypes.Changed));
+				Expect(changed.Name, Is.EqualTo("Changed.Txt"));
+			}
+		}
+
+		[Test]
+		[Timeout(2000)]
+		public async Task CreateFile_StreamsCreated()
+		{
+			using (var watcher = new ObservableFileSystemWatcher(c => { c.Path = TempPath; }))
+			{
+				var firstCreated = watcher.Created.FirstAsync().ToTask();
+				var filePath = Path.Combine(TempPath, "Created.Txt");
+				watcher.Start();
+
+				File.WriteAllText(filePath, "foo");
+
+				var created = await firstCreated;
+				Expect(created.ChangeType, Is.EqualTo(WatcherChangeTypes.Created));
+				Expect(created.Name, Is.EqualTo("Created.Txt"));
+			}
+		}
+
+		[Test]
+		[Timeout(2000)]
+		public async Task DeleteFile_StreamsDeleted()
+		{
+			using (var watcher = new ObservableFileSystemWatcher(c => { c.Path = TempPath; }))
+			{
+				var firstDeleted = watcher.Deleted.FirstAsync().ToTask();
+				var filePath = Path.Combine(TempPath, "ToDelete.Txt");
+				File.WriteAllText(filePath, "foo");
+				watcher.Start();
+
+				File.Delete(filePath);
+
+				var deleted = await firstDeleted;
+				Expect(deleted.ChangeType, Is.EqualTo(WatcherChangeTypes.Deleted));
+				Expect(deleted.Name, Is.EqualTo("ToDelete.Txt"));
+			}
+		}
+
+		[Test]
+		[Timeout(2000)]
+		public async Task DeleteMonitoredDirectory_StreamsError()
+		{
+			using (var watcher = new ObservableFileSystemWatcher(c => { c.Path = TempPath; }))
+			{
+				var firstError = watcher.Errors.FirstAsync().ToTask();
+				watcher.Start();
+
+				Directory.Delete(TempPath);
+
+				var error = await firstError;
+				Expect(error.GetException().Message, Is.EqualTo("Access is denied"));
+			}
+		}
+
+		[Test]
+		[Timeout(2000)]
+		public async Task RenameFile_StreamsRenamed()
+		{
+			using (var watcher = new ObservableFileSystemWatcher(c => { c.Path = TempPath; }))
+			{
+				var firstRenamed = watcher.Renamed.FirstAsync().ToTask();
+				var originalPath = Path.Combine(TempPath, "Changed.Txt");
+				File.WriteAllText(originalPath, "foo");
+				watcher.Start();
+
+				var renamedPath = Path.Combine(TempPath, "Renamed.Txt");
+				File.Move(originalPath, renamedPath);
+
+				var renamed = await firstRenamed;
+				Expect(renamed.OldFullPath, Is.EqualTo(originalPath));
+				Expect(renamed.FullPath, Is.EqualTo(renamedPath));
+			}
+		}
+
+		[TearDown]
+		public void AfterEachTest()
+		{
+			if (!Directory.Exists(TempPath))
+			{
+				return;
+			}
+			Directory.Delete(TempPath, true);
+		}
+	}
+}
