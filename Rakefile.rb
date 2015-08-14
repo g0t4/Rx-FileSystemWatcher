31ce425cfa0c24d0c1df8db80e7bc0d108275baa
Wes Higbee <wes.mcclure@gmail.com>
2015-08-14 16:13:10 -0400

    Fix build process to use new version of albacore

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,17 +3,17 @@ require 'albacore'
 
 task :teamcity => [:build]
 
-msbuild :build do |msb|
-	msb.properties :configuration => :Release
-	msb.targets [:Clean, :Build]
-	msb.solution = 'src/RxFileSystemWatcher.sln'
+build :build do |b|
+	b.prop 'Configuration', 'Release'
+	b.target = [:Clean, :Build]
+	b.sln = 'src/RxFileSystemWatcher.sln'
 end
 
 task :nuget => [:test] do
-	sh "src/.nuget/nuget pack src/RxFileSystemWatcher/RxFileSystemWatcher.csproj /OutputDirectory " + ENV["NuGetDevFeed"] + " -Prop Configuration=Release"
+	sh "src/.nuget/nuget pack src/RxFileSystemWatcher/RxFileSystemWatcher.csproj -Prop Configuration=Release"
 end
 
-nunit :test => [:build] do |cmd|
-	cmd.command = 'src/packages/NUnit.Runners.2.6.3/tools/nunit-console.exe'
-	cmd.assemblies = ['src/Tests/bin/Release/Tests.dll']
+test_runner  :test => [:build] do |tests|
+	tests.exe = 'src/packages/NUnit.Runners.2.6.3/tools/nunit-console.exe'
+	tests.files = ['src/Tests/bin/Release/Tests.dll']
 end
