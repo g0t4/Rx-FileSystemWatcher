52065b428dcb689c8be232e655dbf6bee6e20a85
Ross Dargan <rossdargan@users.noreply.github.com>
2018-02-22 10:19:47 +0000

    Update FileDropWatcher.cs

--- src/RxFileSystemWatcher/FileDropWatcher.cs
+++ src/RxFileSystemWatcher/FileDropWatcher.cs
@@ -7,7 +7,7 @@ namespace RxFileSystemWatcher
 	using System.Reactive.Subjects;
 
 	/// <summary>
-	///	 An observable abstraction to monitor for files dropped into a directory
+	///     An observable abstraction to monitor for files dropped into a directory
 	/// </summary>
 	public class FileDropWatcher : IDisposable
 	{
@@ -60,9 +60,9 @@ namespace RxFileSystemWatcher
 		}
 
 		/// <summary>
-		///	 Use this to scan for files and raise dropped events for any results.
-		///	 This is great to use right after starting the watcher to find existing files.
-		///	 Existing files will trigger dropped events through the Dropped stream.
+		///     Use this to scan for files and raise dropped events for any results.
+		///     This is great to use right after starting the watcher to find existing files.
+		///     Existing files will trigger dropped events through the Dropped stream.
 		/// </summary>
 		public void PollExisting()
 		{
