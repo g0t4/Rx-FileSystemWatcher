0d795ffb5f2592387c60cb377ece9c4d299cd634
TeBeCo <tebeco@gmail.com>
2018-07-03 14:53:04 +0200

    prefer space over tab in C# and using outside of namespace

--- src/RxFileSystemWatcher/FileDropped.cs
+++ src/RxFileSystemWatcher/FileDropped.cs
@@ -1,26 +1,26 @@
-﻿namespace RxFileSystemWatcher
+﻿using System.IO;
+
+namespace RxFileSystemWatcher
 {
-	using System.IO;
+    public class FileDropped
+    {
+        public FileDropped()
+        {
+        }
 
-	public class FileDropped
-	{
-		public FileDropped()
-		{
-		}
+        public FileDropped(FileSystemEventArgs fileEvent)
+        {
+            Name = fileEvent.Name;
+            FullPath = fileEvent.FullPath;
+        }
 
-		public FileDropped(FileSystemEventArgs fileEvent)
-		{
-			Name = fileEvent.Name;
-			FullPath = fileEvent.FullPath;
-		}
+        public FileDropped(string filePath)
+        {
+            Name = Path.GetFileName(filePath);
+            FullPath = filePath;
+        }
 
-		public FileDropped(string filePath)
-		{
-			Name = Path.GetFileName(filePath);
-			FullPath = filePath;
-		}
-
-		public string Name { get; set; }
-		public string FullPath { get; set; }
-	}
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+    }
 }
