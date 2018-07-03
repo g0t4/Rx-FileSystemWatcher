191eaee0c2fef3aa06df136455541f61db200872
TeBeCo <tebeco@gmail.com>
2018-07-03 14:42:52 +0200

    move test file to proper folder and namespace

---
+++ test/RxFileSystemWatcher.Tests/FileIntegrationTestsBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace RxFileSystemWatcher.Tests
+{
+
+    public class FileIntegrationTestsBase : IDisposable
+    {
+        protected string TempPath;
+
+        public FileIntegrationTestsBase()
+        {
+            TempPath = Guid.NewGuid().ToString();
+            Directory.CreateDirectory(TempPath);
+        }
+
+        [TearDown]
+        public void IDispose()
+        {
+            if (!Directory.Exists(TempPath))
+            {
+                return;
+            }
+            Directory.Delete(TempPath, true);
+        }
+    }
+}
