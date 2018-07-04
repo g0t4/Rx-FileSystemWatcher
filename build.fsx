8955bf83fff4813b5264c95d88537669f37366e8
TeBeCo <tebeco@gmail.com>
2018-07-04 14:01:32 +0200

    update .gitignore for VS2017 and run Push only on CI (will use a switch from the CI later)

--- build.fsx
+++ build.fsx
@@ -1,4 +1,4 @@
-#r "paket: 
+#r "paket:
 nuget FSharp.Core
 nuget Fake.Core.Target
 nuget Fake.IO.FileSystem
@@ -36,7 +36,7 @@ let targetTest _ =
   Trace.log " --- Testing projects in parallal --- "
 
   let setDotNetOptions (projectDirectory:string) : (DotNet.TestOptions-> DotNet.TestOptions) =
-    fun (dotNetTestOptions:DotNet.TestOptions) -> 
+    fun (dotNetTestOptions:DotNet.TestOptions) ->
       { dotNetTestOptions with
           Common        = { dotNetTestOptions.Common with WorkingDirectory = projectDirectory}
           Configuration = DotNet.BuildConfiguration.Release
@@ -46,7 +46,7 @@ let targetTest _ =
   !!("test/**/*.Tests.csproj")
   |> Seq.toArray
   |> Array.Parallel.iter (
-    fun fullCsProjName -> 
+    fun fullCsProjName ->
       let projectDirectory = Path.GetDirectoryName(fullCsProjName)
       DotNet.test (setDotNetOptions projectDirectory) ""
     )
@@ -61,7 +61,7 @@ let targetPush _ =
   let source = Environment.environVarOrDefault "NUGET_FEED_TO_PUSH" "NUGET_FEED_TO_PUSH"
   let apiKey = Environment.environVarOrDefault "SOURCE_NUGET_API_KEY" "SOURCE_NUGET_API_KEY"
 
-  let getNugetPushArgs source apiKey= 
+  let getNugetPushArgs source apiKey=
     sprintf "nuget push -s %s -k %s" source apiKey
 
   let nugetPushArgs = getNugetPushArgs source apiKey
@@ -72,7 +72,7 @@ let targetPush _ =
   else
     failwithf "Could not push nuget, error messages :\n%A" processResult
 
-  
+
 
 //*********************************************************/
 // *** Define Targets ***
@@ -94,7 +94,7 @@ open Fake.Core.TargetOperators
   ==> "Build"
   ==> "Test"
   ==> "Pack"
-  =?> ("Push", BuildServer.isLocalBuild)
+  =?> ("Push", not BuildServer.isLocalBuild)
   ==> "Done"
 
 // *** Start Build ***
